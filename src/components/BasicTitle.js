e2202245cd2cd06f23e2ba197a93185c2931e7d6
SnowiWatermelon <heonaliu@gmail.com>
2022-08-02 12:06:51 -0400

    ternary operator.

--- src/components/BasicTitle.js
+++ src/components/BasicTitle.js
@@ -2,6 +2,7 @@ import styled from "styled-components"
 
 const BasicTitle = styled.h1`
   text-align: center;
-  text-transform: capitalize;
+  text-transform: capitalize; 
+  color: ${({special}) => (special ? 'red' : 'blue')}
 `
 export default BasicTitle
